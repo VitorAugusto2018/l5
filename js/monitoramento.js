3bd1e1fc7018f363147b05a9c06446767889a8b9
ti.wdio <ti@wdio.com.br>
2022-10-21 09:41:53 -0300

    melhorias visuais na tela

--- js/monitoramento.js
+++ js/monitoramento.js
@@ -9,15 +9,18 @@ $.ajax({
 
             //console.log(data[i].status)
 
-            $('#cartoes').append(`  <div class="col-sm-4">
-                                        <div class="card" style="width: 18rem;">
-                                            <div class="card-header ${data[i].status}" >
-                                                ${data[i].operador} - (${data[i].nome})                                                                                                                                
+            $('#cartoes').append(`  <div style="margin-left: 20px; margin-bottom: 20px;">
+                                        <div class="card" style="width: 18rem; background-color: #f3f9ff;" >
+                                            <div style="align-self: center;" class="card-header" >
+                                                <img src="./src/${data[i].operador}.png" class="rounded-circle" alt="Cinque Terre" style="align" width="100" height="100"> 
+                                                
                                             </div>
-                                            <div class="card-body">
+                                            <div class="card-body" style="align-self: center;">
                                                 <blockquote class="blockquote mb-0">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                                                    <p>${data[i].operador} - (${data[i].nome})</p>                                                     
+                                                    <footer style="aligtn-self: center">
+                                                        <button type="button" class="${data[i].status} btn btn-lg btn-primary" disabled>${data[i].status}</button>
+                                                    </footer>
                                                 </blockquote>
                                             </div>
                                         </div>
